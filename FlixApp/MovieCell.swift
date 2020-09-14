7f7d64bd9de7cf4a1889376f98fda44080e163e2
Chriscruzz17 <69885930+Chriscruzz17@users.noreply.github.com>
2020-09-14 10:01:27 -0400

    Add files via upload

---
+++ FlixApp/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  FlixApp
+//
+//  Created by christopher cruz on 9/2/20.
+//  Copyright © 2020 Cristopher Cruz. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by christopher cruz on 9/14/20.
+//  Copyright © 2020 Cristopher Cruz. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
